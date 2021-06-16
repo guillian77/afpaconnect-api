cc80aeae9158df40e334cc758ec36036485e795e
Aufrère Guillian <guillian77270@gmail.com>
2021-06-16 12:31:26 +0200

    Feature: implement PHPUNIT

---
+++ tests/RegisterApiTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Tests;
+
+
+use PHPUnit\Framework\TestCase;
+
+class RegisterApiTest extends TestCase
+{
+    public function test()
+    {
+        $this->assertTrue(true);
+    }
+}
