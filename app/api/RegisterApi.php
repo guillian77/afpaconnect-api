92927a8795ddc33dbc30776d7d7023dfbe0d4eaa
Guillian <guillian77270@gmail.com>
2021-08-24 14:07:37 +0200

    feature: implement role verification and registration on register API.

--- app/api/RegisterApi.php
+++ app/api/RegisterApi.php
@@ -4,14 +4,16 @@
 namespace App\Api;
 
 
+use App\Core\JsonWebToken;
 use App\Core\Request;
+use App\Model\App;
+use App\Model\Role;
 use App\Model\User;
 use App\Model\UserRepository;
 use App\Utility\Mailer;
 use App\Utility\Response;
 use App\Utility\StatusCode;
 use Exception;
-use Faker\Factory;
 use Throwable;
 
 /**
@@ -30,12 +32,21 @@ class RegisterApi
 
     private Mailer $mailer;
 
-    public function __construct(UserRepository $repository, Request $request, Response $response, Mailer $mailer)
+    private JsonWebToken $jwt;
+
+    public function __construct(
+        UserRepository $repository,
+        Request $request,
+        Response $response,
+        Mailer $mailer,
+        JsonWebToken $jwt
+    )
     {
         $this->userModel = $repository;
         $this->request = $request;
         $this->response = $response;
         $this->mailer = $mailer;
+        $this->jwt = $jwt;
     }
 
     /**
@@ -45,25 +56,62 @@ class RegisterApi
      */
     public function register()
     {
+        /*
+         * Get and check request parameters.
+         */
         $username = $this->request->get('username');
         $password = $this->request->get('password');
+        $roleIds = $this->request->get('role_id');
 
-        $this->checkParameter($username, "username");
-        $this->checkParameter($password, "password");
+        $this->checkParameter($username, "<username>");
+        $this->checkParameter($password, "<password>");
+        $this->checkParameter($roleIds, "<role> (value: 1,2,3)");
 
+        /*
+         * Try to find user by usernames.
+         */
         /** @var User $user */
         $user = $this->userModel->findOneByUsernames($username);
 
-        $this->checkUserState($user);
+        /*
+         * Check user states: already registered|not exist.
+         */
+        $app = App::where(
+            'name',
+            '=',
+            ucfirst(ucfirst($this->jwt->getIssuer()))
+        )->first();
+
+        $this->checkUserState($user, $app); // Execution can be break here.
 
         $user->password = $password;
 
+        /*
+         * Synchronize user with app and roles.
+         */
+        $roles = Role::whereIn('id', explode(",", $roleIds))->get();
+
+        $newRoles = [];
+        foreach ($roles as $index => $role) {
+            $newRoles[$index]['app_id'] = $app->id;
+            $newRoles[$index]['user_id'] = $user->id;
+            $newRoles[$index]['role_id'] = $role->id;
+        }
+
+        $user->roles()->attach($newRoles);
+
         $userEmail = (empty($user->mail1)) ? $user->mail2 : $user->mail1;
 
+        /*
+         * Generate an activation code.
+         */
         $activationCode = uniqid();
 
         $user->activation_code = $activationCode;
 
+        /*
+         * Send email for account activation and verification.
+         */
         $emailSent = $this->mailer
             ->setTargetEmail($userEmail)
             ->setTargetLastName($user->lastname)
@@ -98,16 +146,31 @@ class RegisterApi
     }
 
     /**
-     * Check if user is already registered or not.
+     * Check if user is already registered and if exist in local database.
      *
-     * @param User $user User
+     * @param User|null $user L'utilisateur trouvé via le pseudonyme.
      */
-    private function checkUserState(User $user):void
+    private function checkUserState(?User $user, ?App $app):void
     {
-        if (
-            !empty($user->password)
-            && !empty($user->activation_code)
-        ) {
+        /*
+         * User exist in local database ?
+         */
+        if (!$user) {
+            $message  = "User not found in our database.";
+            $message .= "User presence should be verified before call this route.";
+            $message .= "If user should exist. Contact administrator.";
+
+            $this->response
+                ->setStatusCode(StatusCode::USER_REGISTER_NOT_FOUND)
+                ->setStatusMessage($message)
+                ->setBodyContent($user)
+                ->send(200, true);
+        }
+
+        /*
+         * User is already on this app ?
+         */
+        if ($user->hasApp($app->id)) {
             $this->response
                 ->setStatusCode(StatusCode::USER_REGISTER_ALREADY)
                 ->setStatusMessage('User is already registered. You have to use route /api/login instead.')
