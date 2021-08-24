cf4db0a6e0321e7bebdda6cfb85e2f1aeb89de82
eloise <eloise.moreau@gmx.com>
2021-08-24 13:43:17 +0200

    fixture/labels corrections

--- public/js/features/role/manage.js
+++ public/js/features/role/manage.js
@@ -38,17 +38,21 @@
         // Generate app role edit Fields
         $('#app_role_edit').find('.single_field').each( (i, app) => {
             
+            app_id = app.getAttribute('id');
+
             roles.forEach(role => {
                 
                 let app_field_option =  document.createElement("div");
                 
                 let app_field_option_label = document.createElement("label");
+                app_field_option_label.setAttribute('for', 'app_' + app_id + '_role_' + role['id']);
                 app_field_option_label.innerHTML = role.name;
 
                 let el = document.createElement("input");
-                el.setAttribute('type','checkbox');
-                el.setAttribute('class','form_field');
-                el.setAttribute('name','app_role_' +app.getAttribute('id')+ '[]');
+                el.type = 'checkbox';
+                el.className ='form_field';
+                el.name = 'app_role_' + app_id + '[]';
+                el.id = 'app_' + app_id+ '_role_' + role['id'];
                 el.value = role['id'];
                 
                 app_field_option.append(el);
@@ -63,9 +67,8 @@
         // Generate Role edit Form
         roles.forEach(role => {
             let role_field = document.createElement("div");
-            role_field.setAttribute('class','single_field' )
-            role_field.setAttribute('name', 'new_role');
-            role_field.setAttribute('id', 'new_role');
+            role_field.className = 'single_field';
+            role_field.name = 'name', 'new_role';
             role_field.id = role['id'];
 
             let role_label = document.createElement('h4');
@@ -75,12 +78,14 @@
             let role_name_field =  document.createElement("div");
 
             let role_name_label = document.createElement('label');
+                role_name_label.setAttribute('for', 'role_name_' + role['id']);
                 role_name_label.innerHTML = 'Nom du rôle';
 
             let role_name_input = document.createElement('input');
-                role_name_input.setAttribute('class','form__field');
-                role_name_input.setAttribute('name', 'role_' + role['id'] +'[]');
-                role_name_input.setAttribute('value', role['name']);
+                role_name_input.className = 'form__field';
+                role_name_input.name = 'role_' + role['id'] +'[]';
+                role_name_input.id = 'role_name_' + role['id'] +'[]';
+                role_name_input.value = role['name'];
 
                 role_name_field.append(role_name_label);
                 role_name_field.append(role_name_input);
@@ -88,15 +93,18 @@
             let role_tag_field =  document.createElement("div");
 
             let role_tag_label = document.createElement('label');
+                role_tag_label.setAttribute('for', 'role_tag' + role['id']);
                 role_tag_label.innerHTML = 'Tag';
                 role_tag_field.append(role_tag_label);
                 
             let role_tag_input = document.createElement('input');
-                role_tag_input.setAttribute('class','form__field');
+                role_tag_input.className = 'form__field';
+                role_tag_input.name =  'role_' + role['id'] + '[]';
+                role_tag_input.id = 'role_tag' + role['id'];
+                role_tag_input.value = role['tag']; 
+                
                 role_tag_field.append(role_tag_input);
 
-                role_tag_input.setAttribute('name', 'role_' + role['id'] + '[]');
-                role_tag_input.setAttribute('value', role['tag']); 
 
                 role_field.append(role_name_field);
                 role_field.append(role_tag_field);
